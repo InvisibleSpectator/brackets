1861c253695be6a9bc7dd68ac564168f434c6ab7
InvisibleSpectator <vladislav5678@yandex.ru>
2019-09-30 19:43:32 +0400

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let i = 0;
+  while (i < str.length) {
+    let j = 0;
+    while (!bracketsConfig[j].includes(str[i]))
+      j++;
+    if (bracketsConfig[j][0] == str[i] && bracketsConfig[j][1] == str[i + 1]) {
+      str = str.slice(0, i) + str.slice(i + 2);
+      i--;
+      if (i < 0)
+      i = 0;
+    }
+    else
+      i++;    
+  }
+  return str.length == 0;
 }
